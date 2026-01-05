c548aca49009008364393c9333260c202c3bba24
𝐌 𝐔 𝐑 𝚨 𝐋 𝐈 𓂃ִֶָ⋆.˚ 🔮 <148250637+Itz-Murali@users.noreply.github.com>
2026-01-05 19:34:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-        const images = ["resources/img/suzume1.png", "resources/img/suzume2.png", "resources/img/suzume3.png", "resources/img/suzume4.png"];
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-        document.getElementById("suzumeImage").src = randomImage;
-    });
